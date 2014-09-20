fa7e5692855660d302b9a702c4966c901d79259b
bobm123 <bobm123@gmail.com>
2014-09-20 09:15:01 -0400

    Flesh out the basic functional test with a class and the beginning of a user story. Also adds some browser cleanup along the way.

--- functional_test.py
+++ functional_test.py
@@ -1,6 +1,49 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get("http://localhost:8000")
+#<< tests run as classes which inherit from unittest.TestCase >>
+class NewVisitorTest(unittest.TestCase):
 
-assert 'Django' in browser.title
+    #<< Next two are special methods and run before each test case >>
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+        
+    #<< main body test method. Has to start test_ >>    
+    def test_startListAndRetreive(self):
+        
+        # Edith has heard about a cool new online to-do app. She goes
+        # to check out its homepage
+        self.browser.get('http://localhost:8000')
+
+        # She notices the page title and header mention to-do lists
+        # << note change from assert to assertIn() >>
+        self.assertIn ('To-Do', self.browser.title)
+        self.fail('Finish the test')
+        
+        # She is invited to enter a to-do item straight away
+
+        # She types "Buy peacock feathers" into a text box (Edith's hobby
+        # is tying fly-fishing lures)
+
+        # When she hits enter, the page updates, and now the page lists
+        # "1: Buy peacock feathers" as an item in a to-do list
+
+        # There is still a text box inviting her to add another item. She
+        # enters "Use peacock feathers to make a fly" (Edith is very methodical)
+
+        # The page updates again, and now shows both items on her list
+
+        # Edith wonders whether the site will remember her list. Then she sees
+        # that the site has generated a unique URL for her -- there is some
+        # explanatory text to that effect.
+
+        # She visits that URL - her to-do list is still there.
+
+        # Satisfied, she goes back to sleep
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
+        
