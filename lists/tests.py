f70a1e58214179adc40ed2ce5a0d1021b55ba98c
bobm123 <bobm123@gmail.com>
2014-09-20 09:40:19 -0400

    add an app for lists with a deliberatly failing test case

---
+++ lists/tests.py
@@ -0,0 +1,11 @@
+from django.test import TestCase
+
+# Create your tests here.
+
+class SmokeTest(TestCase):
+
+    def test_badMaths(self):
+        self.assertEqual(1 + 1, 3.4)
+
+    def test_moreBadMaths(self):
+        self.assertNotEqual(1 + 1, 2)
